32a7b3c8420739bc23dee4f93b3655819f23a901
Pavel Emelyanov <xemul@scylladb.com>
2020-06-10 15:02:07 +0300

    utils: Intrusive B-tree (with tests)
    The design of the tree goes from the row-cache needs, which are
    
    1. Insert/Remove do not invalidate iterators
    2. Elements are LSA-manageable
    3. Low key overhead
    4. External tri-comparator
    5. As little actions on insert/remove as possible
    
    With the above the design is
    
    Two types of nodes -- inner and leaf. Both types keep pointer on parent nodes
    and N pointers on keys (not keys themselves). Two differences: inner nodes have
    array of pointers on kids, leaf nodes keep pointer on the tree (to update left-
    and rightmost tree pointers on node move).
    
    Nodes do not keep pointers/references on trees, thus we have O(1) move of any
    object, but O(logN) to get the tree size. Fortunately, with big keys-per-node
    value this won't result in too many steps.
    
    In turn, the tree has 3 pointers -- root, left- and rightmost leaves. The latter
    is for constant-time begin() and end().
    
    Keys are managed by user with the help of embeddable member_hook instance,
    which is 1 pointer in size.
    
    The code was copied from the B+ tree one, then heavily reworked, the internal
    algorythms turned out to differ quite significantly.
    
    For the sake of mutation_partition::apply_monotonically(), which needs to move
    an element from one tree into another, there's a key_grabber helping wrapper
    that allows doing this move respecting the exception-safety requirement.
    
    As measured by the perf_collections test the B-tree with 8 keys is faster, than
    the std::set, but slower (spoiler: v2 is better than v1 in this sense) than the
    B+tree:
    
                vs set        vs b+tree
       fill:     +13%           -6%
       find:     +23%          -35%
    
    v2:
    - introduce "linear" root leaf that grows on demand
    
      This improves the memory consumption for small trees. This linear node may
      and should over-grow the NodeSize parameter. This comes from the fact that
      there are two big per-key memory spikes on small trees -- 1-key root leaf
      and the first split, when the tree becomes 1-key root with two half-filled
      leaves. If the linear extention goes above NodeSize it can flatten even the
      2nd peak
    
    - mitigate the keys indirection a bit
    
      Prefetching the keys while doing the intra-node linear scan and the nodes
      while descending the tree gives ~+5% of fill and find
    
    - generalize stress tests for B and B+ trees
    
    - cosmetic changes
    
    TODO:
    
    - fix few inefficincies in the core code (walks the sub-tree twice sometimes)
    - optimize 1-key case by embedding the linear node into the tree itself (there's
      room for it in this case, 2-keys already calls for allocated node)
    - try to optimize the leaf nodes, that are not lef-/righmost not to carry
      unused tree pointer on board
    - check if eytzinger trick helps intra-node search
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- test/perf/perf_collections.cc
+++ test/perf/perf_collections.cc
@@ -34,12 +34,46 @@ struct key_compare {
     bool operator()(const per_key_t& a, const per_key_t& b) const noexcept { return a < b; }
 };
 
+struct key_tri_compare {
+    int operator()(const per_key_t& a, const per_key_t& b) const noexcept {
+        if (a > b) {
+            return 1;
+        } else if (a < b) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+};
+
 #include "utils/bptree.hh"
 
 using namespace seastar;
 
 /* On node size 32 and less linear search works better */
 using test_bplus_tree = bplus::tree<per_key_t, unsigned long, key_compare, 4, bplus::key_search::linear>;
+
+#include "utils/intrusive_btree.hh"
+
+class perf_intrusive_key {
+    per_key_t _k;
+public:
+    intrusive_b::member_hook hook;
+
+    explicit perf_intrusive_key(per_key_t v) noexcept : _k(v) {}
+    perf_intrusive_key(perf_intrusive_key&&) noexcept = default;
+    perf_intrusive_key(const perf_intrusive_key&) = delete;
+
+    struct tri_compare {
+        key_tri_compare _cmp;
+        int operator()(const per_key_t a, const per_key_t b) const noexcept { return _cmp(a, b); }
+        int operator()(const perf_intrusive_key& a, const perf_intrusive_key& b) const noexcept { return _cmp(a._k, b._k); }
+        int operator()(const per_key_t a, const perf_intrusive_key& b) const noexcept { return _cmp(a, b._k); }
+        int operator()(const perf_intrusive_key& a, const per_key_t b) const noexcept { return _cmp(a._k, b); }
+    };
+};
+
+using test_b_tree = intrusive_b::tree<perf_intrusive_key, &perf_intrusive_key::hook, perf_intrusive_key::tri_compare, 12, 18, intrusive_b::key_search::linear>;
 
 class collection_tester {
 public:
@@ -84,6 +118,43 @@ public:
         fmt::print("datas:     {}\n", st.datas);
     }
     virtual ~bptree_tester() {
+        _t.clear();
+    }
+};
+
+class btree_tester : public collection_tester {
+    test_b_tree _t;
+    perf_intrusive_key::tri_compare _cmp;
+public:
+    btree_tester() : _t() {}
+    virtual void insert(per_key_t k) override { _t.insert(std::make_unique<perf_intrusive_key>(k), _cmp); }
+    virtual void lower_bound(per_key_t k) override {
+        auto i = _t.lower_bound(k, _cmp);
+        assert(i != _t.end());
+    }
+    virtual void erase(per_key_t k) override { _t.erase_and_dispose(k, _cmp, [] (perf_intrusive_key* k) noexcept { delete k; }); }
+    virtual void drain(int batch) override {
+        int x = 0;
+        auto i = _t.begin();
+        while (i != _t.end()) {
+            i = i.erase_and_dispose([] (perf_intrusive_key* k) noexcept { delete k; });
+            if (++x % batch == 0) {
+                seastar::thread::yield();
+            }
+        }
+    }
+    virtual void show_stats() {
+        struct intrusive_b::stats st = _t.get_stats();
+        fmt::print("nodes:     {}\n", st.nodes);
+        for (int i = 0; i < (int)st.nodes_filled.size(); i++) {
+            fmt::print("   {}: {} ({}%)\n", i, st.nodes_filled[i], st.nodes_filled[i] * 100 / st.nodes);
+        }
+        fmt::print("leaves:    {}\n", st.leaves);
+        for (int i = 0; i < (int)st.leaves_filled.size(); i++) {
+            fmt::print("   {}: {} ({}%)\n", i, st.leaves_filled[i], st.leaves_filled[i] * 100 / st.leaves);
+        }
+    }
+    virtual ~btree_tester() {
         _t.clear();
     }
 };
@@ -179,6 +250,8 @@ int main(int argc, char **argv) {
 
             if (col == "bptree") {
                 c = std::make_unique<bptree_tester>();
+            } else if (col == "btree") {
+                c = std::make_unique<btree_tester>();
             } else if (col == "set") {
                 c = std::make_unique<set_tester>();
             } else if (col == "map") {
