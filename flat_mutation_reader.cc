bc5675b01151ee33c0c0a2c89677f80cfbd115f1
Pavel Emelyanov <xemul@scylladb.com>
2020-06-16 18:54:22 +0300

    flat_mutation_reader: Use clear() in destroy_current_mutation()
    The same reason as in previous patch -- there's not unlink_leftmost_without_rebalance
    method in B-tree, but doing plain clear() is as fast as that way.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- flat_mutation_reader.cc
+++ flat_mutation_reader.cc
@@ -468,11 +468,8 @@ flat_mutation_reader_from_mutations(std::vector<mutation> mutations, const dht::
         }
         void destroy_current_mutation() {
             auto &crs = _cur->partition().clustered_rows();
-            auto re = crs.unlink_leftmost_without_rebalance();
-            while (re) {
-                current_deleter<rows_entry>()(re);
-                re = crs.unlink_leftmost_without_rebalance();
-            }
+            auto deleter = current_deleter<rows_entry>();
+            crs.clear_and_dispose(deleter);
 
             auto &rts = _cur->partition().row_tombstones().tombstones();
             auto rt = rts.unlink_leftmost_without_rebalance();
