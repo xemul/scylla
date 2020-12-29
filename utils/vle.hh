24af8545a03b8b915b3865bd8af00379be111fd8
Pavel Emelyanov <xemul@scylladb.com>
2020-12-29 14:47:39 +0300

    vle: Optimized x86 version
    Current uleb encoder does it's job in a loop. It can be done
    better, but the betterness turned to differ significantly
    depending on the CPU manufactiurer.
    
    First, the optimized version does branch-less encoding just
    by re-packing the given value with the help of classical
    instructions (plus the clz one).
    
    The second optimization can be done if using the bdep instruction
    to replace half of the re-packing code, and this part is good on
    Intel but is bad on AMD.
    
    To compare the encoding routine itself I checked it on Intel i5,
    i7 and Xeon and AMD threadripper chips.
    
    Trivial optimization as compared to the loop version:
    
       Intel: 2.5 times faster
       AMD:   3.0 times faster
    
    The same plus bdep instruction as compared to loop:
    
       Intel: 3.5 times faster
       AMD:   3.8 times slower (!)
    
    The latter seems to be because the bdep is implemented in AMD's
    microcode and slows things down.
    
    So the bdep version is here, but is compiled out by default.
    
    Next some heavier than the femto-benchmarks. All made on the
    AMD Threadripper with non-bdep version.
    
    * the added in this set LSA allocation test shows 15% smaller
    allocation latency in dev mode and 40% smaller in release mode
    
    * perf_collection test in release mode is 3% faster to fill the
    B+tree with 5M keys. In dev mode the difference is within the
    measured deviation.
    
    * perf_mutation test doesn't show measurable difference
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- utils/vle.hh
+++ utils/vle.hh
@@ -29,6 +29,94 @@ static inline size_t uleb32_encoded_size(uint32_t val) noexcept {
     return log2floor(val) / 6;
 }
 
+/*
+ * For ASAN case poisoning helpers are not-trivial and
+ * using optimized encoder doesn't make noticeable profit
+ */
+#if defined(__x86_64__) && !defined(SEASTAR_ASAN_ENABLED)
+
+#ifdef __ARCH_HAS_FAST_PDEP__
+#include <x86intrin.h>
+/*
+ * AMD chips have this instruction implemented in microcode which
+ * gives HUUUUGE performance penalty. On Intel this would give us
+ * ~10% more performance.
+ */
+static inline uint32_t _uleb32_expand(uint32_t val) noexcept {
+    static_assert(uleb32_max_encoded_value <= (1 << 24));
+    return _pdep_u32(val, 0x3f3f3f3f);
+}
+#else
+static inline uint32_t _uleb32_expand(uint32_t val) noexcept {
+    uint32_t ret = val & 0x3f;
+    if constexpr (uleb32_max_encoded_value > 0x3f) {
+        ret |= (val & 0xfc0) << 2;
+        if constexpr (uleb32_max_encoded_value > 0xfc0) {
+            ret |= (val & 0x3f000) << 4;
+            if constexpr (uleb32_max_encoded_value > 0x3f000) {
+                ret |= (val & 0xfc0000) << 6;
+                static_assert(uleb32_max_encoded_value <= 0xfc0000);
+            }
+        }
+    }
+    return ret;
+}
+#endif
+
+template <typename Poison, typename Unpoison>
+static inline void uleb32_encode(char*& pos, uint32_t val, Poison&& poison, Unpoison&& unpoison) noexcept {
+    uint32_t res = _uleb32_expand(val) | 64;
+    /*
+     * The _bit_scan_reverse is slightly better, but needs
+     * the -mbmi2 flag to compile
+     */
+    size_t encoded_size = (32 - __builtin_clz(res)) >> 3;
+    res |= 128 << (encoded_size << 3);
+    /*
+     * This version is used to fill trailing gaps on segments
+     * so mind the encoded_size -- there can be no available
+     * memory after it
+     */
+    memcpy(pos, &res, encoded_size + 1);
+    pos += encoded_size + 1;
+}
+
+template <typename Poison, typename Unpoison>
+static inline void uleb32_encode(char*& pos, uint32_t val, size_t encoded_size, Poison&& poison, Unpoison&& unpoison) noexcept {
+    /*
+     * Most, if not all, of the allocations in LSA come with
+     * alignment <= sizeof(pointer) so this gap can only exceed
+     * 8 bytes in one case -- if the previous allocation ended
+     * up with 1-byte gap and the encoder says it needs 2+ for
+     * the descriptor, then we'll get a 1 + sizeof(pointer) =
+     * = 9-byte gap to fill.
+     *
+     * Also after the descriptor there _will_ come an object,
+     * so we _do_ have at least 8 bytes of memory available.
+     *
+     * Said that in a typicall situation the steps are
+     *  - put the zero at the trailing byte. If it's less than the
+     *    9'th byte it will be overwritten
+     *  - put 8 bytes of the encoded value. This possibly overwrites
+     *    the object's memory, but the object is to be allocated in
+     *    it, so OK
+     *  - bump the "ending" bit in the trailing byte
+     *
+     * This works for gaps of sizes 1 through 9. But just in case
+     * someone uses larger alignment, we need to fill the wider
+     * gap with zeroes
+     */
+
+    uint64_t res = _uleb32_expand(val) | 64;
+    if (encoded_size > sizeof(uint64_t) + 1) [[unlikely]] {
+        memset(pos + sizeof(uint64_t), 0, encoded_size - (sizeof(uint64_t) + 1));
+    }
+    pos[encoded_size - 1] = 0x0;
+    *reinterpret_cast<uint64_t*>(pos) = res;
+    pos[encoded_size - 1] |= 128;
+    pos += encoded_size;
+}
+#else
 template <typename Poison, typename Unpoison>
 static inline void uleb32_encode(char*& pos, uint32_t val, Poison&& poison, Unpoison&& unpoison) noexcept {
     uint64_t b = 64;
@@ -62,6 +150,7 @@ static inline void uleb32_encode(char*& pos, uint32_t val, size_t encoded_size, 
     } while (encoded_size);
     poison(start, pos - start);
 }
+#endif
 
 template <typename Poison, typename Unpoison>
 static inline uint32_t uleb32_decode_forwards(const char*& pos, Poison&& poison, Unpoison&& unpoison) noexcept {
